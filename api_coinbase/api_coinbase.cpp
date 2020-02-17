c5ee60f5ba05a44cb4a078ffe1f700fb69b9a599
Kiko Donda <josedonda@gmail.com>
2020-02-17 18:23:45 +0100

    Actualizados

--- api_coinbase/api_coinbase.cpp
+++ api_coinbase/api_coinbase.cpp
@@ -1,4 +1,4 @@
-// ConsoleApplication1.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// api_coinbase.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 //
 
 #include <iostream>
