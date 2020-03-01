2975f0fa512d756669e3b264758089dc7c3b2203
Kiko Donda <josedonda@gmail.com>
2020-03-01 20:09:18 +0100

    aa

---
+++ Primer_C.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void)
+{
+    printf ("Primer programa C");
+
+    fflush(stdin);
+    printf ("\nPulsa intro para finalizar");
+    getchar ();
+
+}
