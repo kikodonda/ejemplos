a5360d7acaf6e5be2b1bdb8b80029aa5a7b77df8
Kiko Donda <josedonda@gmail.com>
2020-03-01 20:41:26 +0100

    Primer C++

---
+++ Primer_CXX.cpp
@@ -0,0 +1,12 @@
+// Primer programa C++
+#include    <iostream>
+
+int main (void)
+{
+    std::cout <<"Primer programa C++";
+    fflush (stdin);
+    std::cout << "\nPulse intro para finalizar";
+    std::cin.get();
+}
+
+
