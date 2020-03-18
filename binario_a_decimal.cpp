5d0170a5f455b13ed60c12ad744ca858094f3b8f
Kiko Donda <josedonda@gmail.com>
2020-03-18 20:00:39 +0100

    Pasar de binario a decimal

---
+++ binario_a_decimal.cpp
@@ -0,0 +1,30 @@
+/*
+Pasar de binario a decimal en C++
+Programa C++ que pasa un número de binario a decimal. El programa pide un número por teclado en base binaria y calcula y muestra su equivalente base decimal.
+*/
+//Pasar de binario a decimal en C++
+
+// No funciona 
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int exp,digito;
+    double binario, decimal;
+   cout << "Introduce numero: ";
+   cin >> binario;
+   exp=0;
+   decimal=0;
+   while(((int)(binario/10))!=0)
+   {
+           digito = (int)binario % 10;
+           decimal = decimal + digito * pow(2.0,exp);
+           exp++;
+           binario=(int)(binario/10);
+   }
+   decimal=decimal + binario * pow(2.0,exp);
+   cout << endl << "Decimal: " << decimal << endl;
+    system("pause");
+}
