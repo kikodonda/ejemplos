a47dfa715222d23630d515d5d0e0cdcc66a8a6e0
Kiko Donda <josedonda@gmail.com>
2020-03-18 13:18:46 +0100

    Pasar Array a funcion - ejercicio 1

--- pasar_array_a_funcion.cpp
+++ pasar_array_a_funcion.cpp
@@ -1,3 +1,17 @@
+/* Fuente http://ejercicioscpp.blogspot.com/2013/11/pasar-array-a-funcion-parametro-c.html?m=1
+
+Para pasar un array a una función:
+-         En la función se debe declarar el parámetro como un array del tipo adecuado. En los arrays unidimensionales no es necesario especificar 
+          el tamaño  por lo tanto aparecerán los corchetes vacíos.
+-         En la llamada a la función a la que se envía el array, el nombre del array aparecerá solo, sin corchetes ni índices
+Los arrays en C++ se pasan siempre a las funciones por referencia. El nombre de un array realmente es un puntero que contiene 
+la dirección de memoria donde comienza el array, o lo que es lo mismo, la dirección de memoria del primer elemento. Cuando se 
+pasa un array a una función, la función recibe la dirección de memoria donde se encuentra el primer elemento del array. Por lo 
+tanto, la función trabaja sobre el array original.
+No es necesario utilizar el operador & en el paso de arrays a funciones para indicar que se hace por referencia ya que siempre pasan por referencia.
+Una función en C++ no puede devolver un array mediante la instrucción return.
+
+*/
 #include <iostream>
 using namespace std;
 void intercambia(int []); //declaración de la función.
