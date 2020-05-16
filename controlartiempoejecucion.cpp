9f18d9d8dcc194c716409097a2a5e6d94d4e5b4a
kikodonda <josedonda@gmail.com>
2020-05-16 13:48:07 +0200

    Tiempo de ejecucion

---
+++ controlartiempoejecucion.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+unsigned t0, t1;
+
+int main  (){
+
+t0=clock();
+cout <<  "Ahiii anitaaa" << endl;
+t1 = clock();
+ 
+double time = (double(t1-t0)/CLOCKS_PER_SEC);
+cout << "Execution Time: " << time << endl;
+
+return 0;
+}
